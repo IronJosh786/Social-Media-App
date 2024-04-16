84310b820318cba71290fa98f336abed1d4286ac
Faizan <sfaizan78692@gmail.com>
2024-04-16 21:51:18 +0530

    change base url

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,3 +1,3 @@
 // export const base = "http://localhost:3000";
 // export const base = "https://social-media-app-2q9d.onrender.com";
-export const base = "https://social-media-app-production-1221.up.railway.app/";
+export const base = "https://social-media-app-production-1221.up.railway.app";
