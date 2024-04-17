7f1072f0495fc020d8877bd3453bf675d8adaa69
Faizan <sfaizan78692@gmail.com>
2024-04-17 12:23:11 +0530

    change cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://frontend-production-391c.up.railway.app/",
+    origin: "https://frontend-production-391c.up.railway.app",
     credentials: true,
   })
 );
