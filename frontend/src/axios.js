206c64b61c3c707bd2f5546a0452b27cf8dedf24
Faizan <sfaizan78692@gmail.com>
2024-04-17 13:12:31 +0530

    add console in axios configuration

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -4,6 +4,7 @@ import Cookie from "js-cookie";
 const instance = axios.create();
 
 const accessToken = Cookie.get("access_token");
+console.log("access_token: ", accessToken);
 
 if (accessToken) {
   instance.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
