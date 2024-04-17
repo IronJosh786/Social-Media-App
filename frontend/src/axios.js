f0254f9de606a05760ab1ca62e16d7c4f8ac3b7d
Faizan <sfaizan78692@gmail.com>
2024-04-17 14:49:43 +0530

    fix cookies typo

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
-import Cookie from "js-cookie";
+import Cookies from "js-cookie";
 
 const instance = axios.create();
 
 const setHeader = (access_token) => {
-  const accessToken = Cookie.get("access_token") || access_token;
+  const accessToken = Cookies.get("access_token") || access_token;
   if (accessToken) {
     instance.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
   } else {
