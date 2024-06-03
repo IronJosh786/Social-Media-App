7ec935c37ccfee86be3f08d3d4e2fedc65efc81e
Faizan <sfaizan78692@gmail.com>
2024-06-03 18:19:05 +0530

    change base url

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,2 +1,2 @@
-// export const base = "https://social-media-app-three-nu.vercel.app";
-export const base = "http://localhost:3000";
+export const base = "https://social-media-app-three-nu.vercel.app";
+// export const base = "http://localhost:3000";
